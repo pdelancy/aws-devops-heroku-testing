b8d042182e5b7f246d214b3f21268cbdaf252e02
pdelancy <pdelancy94@gmail.com>
2018-04-18 15:13:20 -0700

    some stuff

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var router = express.Router()
 
 router.get("/", function(req, res, next) {
-	res.render("index", { title: "Node.js on Heroku" })
+    res.render("index", { title: "Moose's very special app" })
 })
 
 module.exports = router
